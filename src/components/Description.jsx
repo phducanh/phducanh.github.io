5b25461bd984317017b669d28bcb7598a029c657
Galvin Pham <anh@save.day>
2025-03-15 00:45:48 +0700

    fix cicd

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,30 +1,50 @@
+import descriptionData from '../../data/description.json';
+
 const Description = () => {
     return (
         <>
             <p>
-                My life, where each week I've been alive is a little box. When meaningful things happen (for
-                both better and worse) I make a note of it so I can remember how much of life is influenced
-                by these factors. This was originally created by Buster Benson, and if you'd like to create
-                your own version of this, it's basically a <a
-                    href="https://github.com/busterbenson/notes/blob/master/_data/life-in-weeks.yml"
-                    target="_blank" rel="noopener noreferrer"
-                    className="external-link">data file</a>, a <a
-                    href="https://github.com/busterbenson/notes/blob/master/_layouts/life-in-weeks.html"
-                    target="_blank" rel="noopener noreferrer"
-                    className="external-link">template</a>, and a <a
-                    href="https://github.com/busterbenson/notes/blob/master/_pages/life-in-weeks.md"
-                    target="_blank" rel="noopener noreferrer"
-                    className="external-link">blog post</a> mashed together on <a
-                    href="https://jekyllrb.com/" target="_blank"
-                    rel="noopener noreferrer"
-                    className="external-link">Jekyll</a> blog hosted on <a
-                    href="https://github.com/" target="_blank" rel="noopener noreferrer"
-                    className="external-link">Github</a>.
+                {descriptionData.mainText}{' '}
+                {descriptionData.links.map((link, index) => (
+                    <>
+                        <a
+                            key={index}
+                            href={link.url}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="external-link"
+                        >
+                            {link.text}
+                        </a>
+                        {index < descriptionData.links.length - 1 ? ', a ' : ' '}
+                    </>
+                ))}
+                {descriptionData.additionalText}{' '}
+                {descriptionData.platformLinks.map((link, index) => (
+                    <>
+                        <a
+                            key={index}
+                            href={link.url}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="external-link"
+                        >
+                            {link.text}
+                        </a>
+                        {index < descriptionData.platformLinks.length - 1 ? ' blog hosted on ' : '.'}
+                    </>
+                ))}
             </p>
             <p>
-                My blog: <a href="https://mayvees.substack.com/" target="_blank"
+                My blog:{' '}
+                <a
+                    href={descriptionData.blog.url}
+                    target="_blank"
                     rel="noopener noreferrer"
-                    className="external-link">Mayvees on Substack</a>
+                    className="external-link"
+                >
+                    {descriptionData.blog.text}
+                </a>
             </p>
         </>
     );
