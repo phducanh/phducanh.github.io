502978fc814dc4182141d010d4cedd44daf3ffb2
Galvin Pham <anh@save.day>
2025-03-14 21:38:17 +0700

    almost finished

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -29,7 +29,9 @@ function Tooltip({
 function TooltipTrigger({
   ...props
 }: React.ComponentProps<typeof TooltipPrimitive.Trigger>) {
-  return <TooltipPrimitive.Trigger data-slot="tooltip-trigger" {...props} />
+  return <TooltipPrimitive.Trigger 
+  className="bg-transparent"
+  data-slot="tooltip-trigger" {...props} />
 }
 
 function TooltipContent({
