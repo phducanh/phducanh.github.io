959e4b33dadd8035b0dd2e96e2ac46877cfbda50
Galvin Pham <anh@save.day>
2025-03-15 02:15:50 +0700

    vite

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/mayvees/',
   plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name].[hash][extname]',
-        chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js',
-      },
-    },
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
