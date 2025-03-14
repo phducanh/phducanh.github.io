ee2c56ca26aa2aedb4a8272fed1eb76790686c76
Galvin Pham <anh@save.day>
2025-03-15 01:27:51 +0700

    fix base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/mayvees/',
   plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
